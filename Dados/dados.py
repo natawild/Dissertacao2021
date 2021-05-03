899ee5eb4cccd3c88244acb7a4d4b3e3c16a1095
Célia Figueiredo <celianlfg@hotmail.com>
2021-05-03 23:41:22 +0100

    dados

--- Dados/dados.py
+++ Dados/dados.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 dadosDelirium = pd.read_csv("dados.csv")
 
-#print(dadosDelirium.head())
+print(dadosDelirium.head())
 
 nan = dadosDelirium[dadosDelirium.isna().any(axis=1)]
 print(nan.head())
