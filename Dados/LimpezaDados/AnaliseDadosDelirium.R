26a4baf6f3dc51ef2930bab73bc7628d7f08ab79
Célia Figueiredo <celianlfg@hotmail.com>
2021-11-11 12:33:58 +0000

    descrição da base de dados

--- Dados/LimpezaDados/AnaliseDadosDelirium.R
+++ Dados/LimpezaDados/AnaliseDadosDelirium.R
@@ -2,12 +2,72 @@ library(readxl)
 library(naniar)
 library(visdat)
 library(ggplot2)
-library(ggplot)
+library(dplyr)
 library(DataExplorer)
+library(corrplot)
 
-DadosAposRevisao <- read_excel("DadosAposRevisao.xlsx", n_max=435)
-View(DadosAposRevisao)   
+
+DadosAposRevisao <- read_excel("DadosAposRevisao.xlsx", n_max = 435)
 attach(DadosAposRevisao)
+
+# Tranformação em factor: 
+DadosAposRevisao$Genero <- factor(DadosAposRevisao$Genero, label = c("M", "F"), levels = c(0, 1))
+
+DadosAposRevisao$Rosuvastatina <- factor(DadosAposRevisao$Rosuvastatina, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Atorvastatina <- factor(DadosAposRevisao$Atorvastatina, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Pravastatina <- factor(DadosAposRevisao$Pravastatina, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Sinvastatina <- factor(DadosAposRevisao$Sinvastatina, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Fluvastatina <- factor(DadosAposRevisao$Fluvastatina, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Alprazolam <- factor(DadosAposRevisao$Alprazolam, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Captopril <- factor(DadosAposRevisao$Captopril, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Desloratadine <- factor(DadosAposRevisao$Desloratadine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Diazepam <- factor(DadosAposRevisao$Diazepam, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Lorazepam <- factor(DadosAposRevisao$Lorazepam, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Digoxin <- factor(DadosAposRevisao$Digoxin, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Dipyridamole <- factor(DadosAposRevisao$Dipyridamole, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Furosemide <- factor(DadosAposRevisao$Furosemide, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Fluvoxamine <- factor(DadosAposRevisao$Fluvoxamine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Haloperidol <- factor(DadosAposRevisao$Haloperidol, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Hydrocortisone <- factor(DadosAposRevisao$Hydrocortisone, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Iloperidone <- factor(DadosAposRevisao$Iloperidone, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Morphine <- factor(DadosAposRevisao$Morphine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Nifedipine <- factor(DadosAposRevisao$Nifedipine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Paliperidone <- factor(DadosAposRevisao$Paliperidone, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Prednisone <- factor(DadosAposRevisao$Prednisone, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Ranitidine <- factor(DadosAposRevisao$Ranitidine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Risperidone <- factor(DadosAposRevisao$Risperidone, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Trazodone <- factor(DadosAposRevisao$Trazodone, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Venlafaxine <- factor(DadosAposRevisao$Venlafaxine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Warfarin <- factor(DadosAposRevisao$Warfarin, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Amitriptyline <- factor(DadosAposRevisao$Amitriptyline, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Hydroxyzine <- factor(DadosAposRevisao$Hydroxyzine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Paroxetine <- factor(DadosAposRevisao$Paroxetine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Quetiapine <- factor(DadosAposRevisao$Quetiapine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Scopolamine <- factor(DadosAposRevisao$Scopolamine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Trihexyphenidyl <- factor(DadosAposRevisao$Trihexyphenidyl, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Clonidine <- factor(DadosAposRevisao$Clonidine, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Sertralina <- factor(DadosAposRevisao$Sertralina, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Tramadol <- factor(DadosAposRevisao$Tramadol, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Mexazolam <- factor(DadosAposRevisao$Mexazolam, label = c("N", "S"), levels = c(0, 1))
+DadosAposRevisao$Trospium <- factor(DadosAposRevisao$Trospium, label = c("N", "S"), levels = c(0, 1))
+
+# Transformação de data de óbito em factor:
+DadosAposRevisao$Obito <- factor(DadosAposRevisao$Obito, label = c("N", "S"), levels = c(0, 1))
+
+# Transformação de alcoolico em factor:
+DadosAposRevisao$Alcoolico <- factor(DadosAposRevisao$Alcoolico, label = c("N", "S"), levels = c(0, 1))
+
+
+# Transformação do resultado de delirium para factor:
+DadosAposRevisao$ResultDelirium <- factor(DadosAposRevisao$ResultDelirium, label = c("Sem delirium", "Delirium"), levels = c(0, 1))
+
+
+
+# Transformando Grupo de diagnostico em factor:
+DadosAposRevisao$GrupoDiagn <- factor(DadosAposRevisao$GrupoDiagn,
+                                  label = c("Neurologico", "Cardiovascular", "Gastrointestinal", "Respiratório", "Genitourinário", "Musculoesquelético", "Toxicidade de Drogas", "Outro", "Hemato-Oncológico"),
+                                  levels = 0:8)
+
 vis_dat(DadosAposRevisao)
 vis_miss(DadosAposRevisao, warn_large_data= FALSE)
 
@@ -37,33 +97,29 @@ library(pacman)
 pacman::p_load(dplyr, psych, car, MASS, DescTools, QuantPsyc, ggplot2)
 
 
-# Passo 2: Carregar o banco de dados
+# Passo 2: Carregar dados
 
 # Importante: selecionar o diretório de trabalho (working directory)
 # Isso pode ser feito manualmente: Session > Set Working Directory > Choose Directory
 
-dados <- read_excel("DadosAposRevisao.xlsx", n_max=435)
-
-View(dados)                                 # Visualização dos dados em janela separada
-glimpse(dados)                              # Visualização de um resumo dos dados
-
+dados <- read_excel("DadosAposRevisao.xlsx", n_max = 435)
+View(dados)               # Visualização dos dados em janela separada
+glimpse(dados)            # Visualização de um resumo dos dados
 
 
 # Passo 3: Análise das frequências das categorias da VD
 
-table(dados$Cancer)
-
+table(dados$ResultDelirium)
 summary(dados)
 
 
-# Passo 4: Checagem das categorias de referência
+# Passo 4: Verificação das categorias de referência
 
-levels(dados$Cancer)  # Não = categoria de referência
-
-levels(dados$Hab_Fumar)  # Não = categoria de referência
+levels(dados$ResultDelirium)  # Não = categoria de referência
 
 
-# Passo 5: Checagem dos pressupostos
+
+# Passo 5: Verificação dos pressupostos
 
 ## 1. Variável dependente dicotômica (categorias mutuamente exclusivas)
 ## 2. Independência das observações (sem medidas repetidas)
@@ -71,7 +127,12 @@ levels(dados$Hab_Fumar)  # Não = categoria de referência
 
 ## Construção do modelo:
 
-mod <- glm(Cancer ~ Estresse + Hab_Fumar,
+mod <- glm(ResultDelirium ~ Idade + Genero + Interna_Dias + GrupoDiagn + Glicose + Sodio + Ureia 
+           + Creatinina + PCR + Rosuvastatina + Atorvastatina + Pravastatina + Sinvastatina + Fluvastatina + Alprazolam
+           + Captopril + Desloratadine + Diazepam + Lorazepam + Digoxin + Dipyridamole + Furosemide + Fluvoxamine + Haloperidol
+           + Hydrocortisone + Iloperidone + Morphine + Nifedipine + Paliperidone + Prednisone + Ranitidine + Risperidone + Trazodone
+           + Venlafaxine + Warfarin + Amitriptyline + Hydroxyzine + Paroxetine + Quetiapine + Scopolamine + Trihexyphenidyl + Clonidine 
+           + Sertralina + Tramadol + Mexazolam + Trospium + Obito + Alcoolico,
            family = binomial(link = 'logit'), data = dados)
 
 
@@ -79,12 +140,12 @@ mod <- glm(Cancer ~ Estresse + Hab_Fumar,
 
 plot(mod, which = 5)
 
-summary(stdres(mod))
 
 
 ## 4. Ausência de multicolinearidade
 
 pairs.panels(dados)
+
 ### Multicolinearidade: r > 0.9 (ou 0.8)
 
 
@@ -95,13 +156,41 @@ vif(mod)
 ## 5. Relação linear entre cada VI contínua e o logito da VD
 
 
+# Passo 7 (opcional): Matrizes de correlação
+
+## Criando a matriz:
+matriz <- cor(dados[1:49], method = "pearson")
+View(matriz)
+
+## Arredondando para duas casas decimais:
+matriz <- round(cor(dados[1:49], method = "pearson"), 2)
+View(matriz)
+
+
+## Criando uma matriz visual (pacote corrplot)
+corrplot(matriz, method = "number")
+
+
+### Opções de métodos: method = circle, color, pie
+### Opções de tipos: type = upper, lower
+### Ordenar: order = hclust
+
+corrplot(matriz, method="color", 
+         type="upper", order="hclust", 
+         addCoef.col = "black", # adiciona o coeficiente à matriz
+         tl.col="black", tl.srt=45, # cor e rotação do nome das variáveis
+         diag=FALSE # não mostrar a diagonal principal
+)
+
+
+
 ### Interação entre a VI contínua e o seu log não significativa (Box-Tidwell)
 
-intlog <- dados$Estresse * log(dados$Estresse)
+intlog <- dados$Idade * log(dados$Idade)
 
 dados$intlog <- intlog
 
-modint <- glm(Cancer ~ Hab_Fumar + Estresse + intlog,
+modint <- glm(ResultDelirium ~ Idade + Genero + intlog,
               family = binomial(link = 'logit'), data = dados)
 
 summary(modint)
@@ -122,7 +211,7 @@ dados$logito <- logito
 
 #### Análise da relação linear
 
-ggplot(dados, aes(logito, Estresse)) +
+ggplot(dados, aes(logito, Idade)) +
   geom_point(size = 0.5, alpha = 0.5) +
   geom_smooth(method = "loess") +
   theme_classic()
@@ -153,7 +242,7 @@ exp(cbind(OR = coef(mod), confint.default(mod)))
 
 # Passo 7: Criação e análise de um segundo modelo
 
-mod2 <- glm(Cancer ~ Hab_Fumar,
+mod2 <- glm(ResultDelirium ~ Idade,
             family = binomial(link = 'logit'), data = dados)
 
 
@@ -197,22 +286,8 @@ anova(mod2, mod, test="Chisq")
 
 
 # Tabela de classificação
-ClassLog(mod, dados$Cancer)
-ClassLog(mod2, dados$Cancer)
+ClassLog(mod, dados$ResultDelirium)
+ClassLog(mod2, dados$ResultDelirium)
 
 
-
-####### Como modificar as categorias de referência? ########
-
-levels(dados$Hab_Fumar)
-
-dados$Hab_Fumar <- relevel(dados$Hab_Fumar, ref = "Sim")
-
-
-### ATENÇÃO: é necessário rodar o modelo novamente!
-
-
-levels(dados$Cancer)
-
-dados$Cancer <- relevel(dados$Cancer, ref = "Sim")
 
